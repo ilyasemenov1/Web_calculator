3fe38170f2146a5b412769d17a5a3857883eb467
Ilya Semenov <84145345+ilyasemenov1@users.noreply.github.com>
2022-06-17 23:45:10 +0300

    Calc bugfix

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 const output = document.querySelector('.calc__output');
 const buttonarea = document.querySelector(".buttonarea");
 
-document.querySelectorAll('.buttonarea__button').forEach(button => {
-    button.addEventListener('click', function () {
-        calc(this.value);
-    })
-})
-
 window.addEventListener("load", function() {
     generate_buttons();
-});
 
-document.addEventListener('keydown', event => {
-    if ((event.key).match(/[0-9%\/*\-+<>.^\(\)=]|Backspace|Enter|Delete/)) {
-        calc(event.key);
-    }
-})
+    document.querySelectorAll('.buttonarea__button').forEach(button => {
+        button.addEventListener('click', function () {
+            calc(this.value);
+        })
+    })
+    
+    document.addEventListener('keydown', event => {
+        if ((event.key).match(/[0-9%\/*\-+<>.^\(\)=]|Backspace|Enter|Delete/)) {
+            calc(event.key);
+        }
+    })
+});
 
 function calc(value) {
     if (value.match(/=|Enter/)) {
@@ -50,16 +50,16 @@ function generate_buttons() {
         "C", "CE", "%", "/", "1", "2", "3", "*", "4", "5", "6", "+", "7", "8", "9", "-", "0", "(", ")", "="
     ];
     for (let i = 0; i < button_values_arr.length; i++) {
-        const button = document.createElement("button");
-        button.className = "buttonarea__button";
-        button.textContent = button_values_arr[i];
+        const button_element = document.createElement("button");
+        button_element.className = "buttonarea__button";
+        button_element.textContent = button_values_arr[i];
 
         if (button_values_arr[i] == "CE") {
-            button.value = "Backspace";
+            button_element.value = "Backspace";
         } else {
-            button.value = button_values_arr[i];
+            button_element.value = button_values_arr[i];
         }
 
-        buttonarea.append(button)
+        buttonarea.append(button_element);
     }
 }
